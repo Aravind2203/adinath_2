6b4a6fe79898e3cb0f8527335744984f692c0c1e
Aravind Swaminathan <71716685+Aravind2203@users.noreply.github.com>
2021-01-10 18:23:04 +0530

    Update urls.py

--- adinathproject/urls.py
+++ adinathproject/urls.py
@@ -15,28 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import handler404,handler500
 from  django.conf.urls.static import static
 from django.conf import settings
-from mainapp.views import home,add,addtobase,detail,product_page
+from mainapp.views import home,addtobase,detail,product_page
 from mainapp import views
 
 urlpatterns = [
     path('adminforadinathenterprise/', admin.site.urls),
     path('',home,name='home'),
-   
     path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
     path('cart/item_clear/<int:id>/', views.item_clear, name='item_clear'),
-    path('cart/item_increment/<int:id>/',
-         views.item_increment, name='item_increment'),
-    path('cart/item_decrement/<int:id>/',
-         views.item_decrement, name='item_decrement'),
+    path('cart/item_increment/<int:id>/',views.item_increment, name='item_increment'),
+    path('cart/item_decrement/<int:id>/',views.item_decrement, name='item_decrement'),
     path('cart/cart_clear/', views.cart_clear, name='cart_clear'),
     path('cart/cart-detail/',views.cart_detail,name='cart_detail'),
-    path('hf/<category>/',views.hf,name='hf'),
     path('addtodatabase/',addtobase,name="addtodatabase"),
     path('detail/<int:id>',detail,name="detail"),
     path('product/<str:category>/',product_page,name='products'),
     path('search/',views.search,name='search'),
+    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 handler404='mainapp.views._404_page'
+handler500='mainapp.views._500_page'
